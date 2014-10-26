54bc8f0ce07c5392b9048a85370b23fd81985cc3
Hannes Junnila <hannes.junnila@gmail.com>
2014-10-26 13:53:30 +0200

    Inital commit

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/hannes/sujuvuusnavigaattori/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/hannes/sujuvuusnavigaattori/platforms/android/CordovaLib/AndroidManifest.xml \
